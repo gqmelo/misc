a7ac4e90377aab307f7530eebad6b9d0e02d7858
Guilherme Quentel Melo <gqmelo@gmail.com>
2016-03-24 03:19:41 -0300

    New patch and tests in C

--- glx_leak/test_glx_leak.py
+++ glx_leak/test_glx_leak.py
@@ -5,8 +5,8 @@ script_filename = os.path.join(os.path.dirname(__file__), 'glx_leak', 'create_gl
 env = os.environ.copy()
 env['LIBGL_ALWAYS_INDIRECT'] = 1
 
-glx_process_1 = pexpect.spawn('python {}'.format(script_filename))
-glx_process_2 = pexpect.spawn('python {}'.format(script_filename))
+glx_process_1 = pexpect.spawn('./quad')
+glx_process_2 = pexpect.spawn('./quad')
 
 glx_process_1.expect('Press Enter to change context')
 glx_process_2.expect('Press Enter to change context')
