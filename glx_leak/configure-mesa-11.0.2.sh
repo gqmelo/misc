d013922be286ae19047b0cb7fb27fc421944a5be
Guilherme Quentel Melo <guilherme@esss.com.br>
2016-03-29 20:38:24 -0300

    Some files submitted to the mesa bug report

---
+++ glx_leak/configure-mesa-11.0.2.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+../../configure --prefix=$HOME/local --mandir=\${prefix}/share/man \
+     --enable-debug \
+     --infodir=\${prefix}/share/info --sysconfdir=\${prefix}/etc \
+     --libdir=\${prefix}/lib/x86_64-linux-gnu \
+     --localstatedir=\${prefix}/var --disable-silent-rules \
+     --build=x86_64-linux-gnu --enable-dri --with-dri-drivers=" nouveau i915 i965 r200 radeon" \
+     --with-dri-driverdir=\${prefix}/lib/dri \
+     --with-dri-searchpath='\${prefix}/lib/dri:\$${ORIGIN}/dri' \
+     --disable-osmesa --enable-glx-tls --enable-shared-glapi --enable-texture-float \
+     --disable-xvmc --disable-omx --enable-driglx-direct --enable-dri3 \
+     --with-egl-platforms="x11 wayland drm" --enable-xa --enable-gallium-llvm \
+     ac_cv_path_LLVM_CONFIG=llvm-config-3.6 --enable-vdpau \
+     --with-gallium-drivers=" nouveau svga r600 r300 radeonsi swrast" \
+     --enable-gles1 --enable-gles2 \
+     CFLAGS="-g -O0 -fstack-protector-strong --param=ssp-buffer-size=4 -Wformat -Werror=format-security -Wall" \
+     CPPFLAGS="-D_FORTIFY_SOURCE=2" \
+     CXXFLAGS="-g -O0 -fstack-protector-strong --param=ssp-buffer-size=4 -Wformat -Werror=format-security -Wall" \
+     FFLAGS="-g -O0 -fstack-protector-strong --param=ssp-buffer-size=4" \
+     GCJFLAGS="-g -O0 -fstack-protector-strong --param=ssp-buffer-size=4" \
+     LDFLAGS="-Wl,-Bsymbolic-functions -Wl,-z,relro"
