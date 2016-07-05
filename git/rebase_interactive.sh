194879e7b12289f0ca4325e6aea28d0a66531d6a
Guilherme Quentel Melo <guilherme@esss.com.br>
2016-07-05 18:48:42 -0300

    Improve rebase script to check if there is something to do

--- git/rebase_interactive.sh
+++ git/rebase_interactive.sh
@@ -8,7 +8,11 @@ fi
 
 echo "merge-base branch: $MERGE_BASE_BRANCH"
 MERGE_BASE=`git merge-base $MERGE_BASE_BRANCH HEAD`
-echo "Rebasing onto commit:"
-echo $MERGE_BASE
-
-git rebase -i $MERGE_BASE
+HEAD=`git rev-parse HEAD`
+if [ "$MERGE_BASE" = "$HEAD" ]; then
+    echo "Skipping rebase HEAD and merge base are the same: $HEAD"
+else
+    echo "Rebasing onto commit:"
+    echo $MERGE_BASE
+    git rebase -i $MERGE_BASE
+fi
