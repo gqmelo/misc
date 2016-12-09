b4b71c08d2b84740f8624bc132621a366c961d0c
Guilherme Quentel Melo <guilherme@esss.com.br>
2016-12-09 11:20:22 -0200

    color git prompt

--- scripts/fish/functions/fish_prompt.fish
+++ scripts/fish/functions/fish_prompt.fish
@@ -65,12 +65,7 @@ function fish_prompt --description 'Write out the prompt'
 		set prompt_status ' ' (set_color $fish_color_status) "[$last_status]" "$normal"
 	end
 
-  # echo -n '['
-  # set_color normal
-  # set_color $retc
-  # echo -n (date +%X)
-  # set_color -o green
-  # echo -n ]
-
+    set -g __fish_git_prompt_show_informative_status 1
+    set -g __fish_git_prompt_showcolorhints 1
 	echo -n -s (set_color normal) [(date +%X)] ' ' (set_color $fish_color_user) "$USER" $normal @ (set_color $fish_color_host) "$__fish_prompt_hostname" $normal ' ' (set_color $color_cwd) (prompt_pwd) $normal (__fish_vcs_prompt) $normal $prompt_status "> "
 end
