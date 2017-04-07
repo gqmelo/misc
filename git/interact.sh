438545ad5ee388ef77bfdd395a42bbf0bdacb761
Guilherme Quentel Melo <guilherme@esss.com.br>
2017-04-07 09:15:55 -0300

    Improve git interaction with fzf

--- git/interact.sh
+++ git/interact.sh
@@ -1,3 +1,13 @@
 #!/bin/sh
 
-cat - | fzf --ansi --preview-window=up --preview='git show --color {1}'
+fzf_git_show() {
+    fzf -0 --ansi --preview-window=up --preview='git show --color {1}'
+}
+
+if [ -t 0 ]; then
+    # Terminal input (keyboard) - interactive
+    git "$@" | fzf_git_show
+else
+    # input from pipe
+    cat - | fzf_git_show
+fi
