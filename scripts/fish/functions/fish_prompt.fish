11240809e97aeeba57c32e69575bc695ffa4a894
Guilherme Quentel Melo <guilherme@esss.com.br>
2017-04-07 13:38:55 -0300

    Refactor notification for long commands

--- scripts/fish/functions/fish_prompt.fish
+++ scripts/fish/functions/fish_prompt.fish
@@ -1,20 +1,7 @@
 function fish_prompt --description 'Write out the prompt'
 	set -l last_status $status
-
-	# If commands runs >= 10 seconds, notify user on completion
-	if test $CMD_DURATION
-		if test $CMD_DURATION -gt (math "1000 * 10")
-			if test $last_status -ne 0
-				set notify_icon 'dialog-error'
-			else
-				set notify_icon 'emblem-default'
-			end
-			set secs (math "$CMD_DURATION / 1000")
-			notify-send -i $notify_icon "$history[1]" "Returned $last_status, took $secs seconds"
-			# Avoid repeating the message (when the window is resized for example)
-			set -gx CMD_DURATION 0
-		end
-	end
+	# Show notification if the last command took too much time
+	long_command_notify
 
 	# Just calculate this once, to save a few cycles when displaying the prompt
 	if not set -q __fish_prompt_hostname
---
+++ scripts/fish/functions/long_command_notify.fish
@@ -0,0 +1,15 @@
+function long_command_notify --description 'Show a desktop notification depending on last command duration'
+	set -l last_status $status
+	if not test $CMD_DURATION; return; end
+	if test $CMD_DURATION -gt (math "1000 * 10")
+		if test $last_status -ne 0
+			set notify_icon 'dialog-error'
+		else
+			set notify_icon 'emblem-default'
+		end
+		set secs (math "$CMD_DURATION / 1000")
+		notify-send -i $notify_icon "$history[1]" "Returned $last_status, took $secs seconds"
+		# Avoid repeating the message (when the window is resized for example)
+		set -gx CMD_DURATION 0
+	end
+end
