70f484c4df13461ef556d63f863b527ad720d2af
Guilherme Quentel Melo <gqmelo@gmail.com>
2017-04-09 03:25:57 -0300

    Copy fzf output to clipboard

--- scripts/fish/functions/fish_user_key_bindings.fish
+++ scripts/fish/functions/fish_user_key_bindings.fish
@@ -1,3 +1,3 @@
 
-set -gx FZF_DEFAULT_OPTS $FZF_DEFAULT_OPTS '--bind alt-j:preview-page-down --bind alt-k:preview-page-up'
+set -gx FZF_DEFAULT_OPTS '--bind alt-j:preview-page-down --bind alt-k:preview-page-up --bind ctrl-l:select-all --bind ctrl-n:deselect-all --bind "enter:execute(echo {+} | xsel -b)+accept"'
 fzf_key_bindings
