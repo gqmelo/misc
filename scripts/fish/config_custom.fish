f40886b6ec7a2d4de1d645f86eb2153e70c416b1
Guilherme Quentel Melo <guilherme@esss.com.br>
2017-04-10 12:23:08 -0300

    Copy paths to clipboard

--- scripts/fish/config_custom.fish
+++ scripts/fish/config_custom.fish
@@ -1,2 +1,3 @@
+set -gx PATH ~/workspace/misc/bin $PATH
 eval (python -m virtualfish)
 complete --command mu --wraps git
--- scripts/fish/functions/fish_user_key_bindings.fish
+++ scripts/fish/functions/fish_user_key_bindings.fish
@@ -1,5 +1,6 @@
 
 set -gx FZF_DEFAULT_OPTS '--bind alt-j:preview-page-down --bind alt-k:preview-page-up --bind ctrl-l:select-all --bind ctrl-n:deselect-all --bind "enter:execute(echo -n {+} | xsel -i -b)+accept"'
+set -gx FZF_CTRL_T_OPTS '--bind "enter:execute(echo -n {+} | xargs copy-path-to-clipboard)+accept"'
 fzf_key_bindings
 
 # Show history with fzf and copy the selection to clipboard
