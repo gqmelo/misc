775359a177689ec9702d4b38ffcee8fd8e980443
Guilherme Quentel Melo <guilherme@esss.com.br>
2017-04-10 11:25:20 -0300

    fiz fzf copy selection

--- scripts/fish/functions/fish_user_key_bindings.fish
+++ scripts/fish/functions/fish_user_key_bindings.fish
@@ -1,6 +1,6 @@
 
-set -gx FZF_DEFAULT_OPTS '--bind alt-j:preview-page-down --bind alt-k:preview-page-up --bind ctrl-l:select-all --bind ctrl-n:deselect-all --bind "enter:execute(echo {+} | xsel -b)+accept"'
+set -gx FZF_DEFAULT_OPTS '--bind alt-j:preview-page-down --bind alt-k:preview-page-up --bind ctrl-l:select-all --bind ctrl-n:deselect-all --bind "enter:execute(echo -n {+} | xsel -i -b)+accept"'
 fzf_key_bindings
 
-# Show history with fzf e copy the selection to clipboard
+# Show history with fzf and copy the selection to clipboard
 bind \er "history | fzf -m | xsel -b"
