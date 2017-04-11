0c578e47f2bf67d5e0c54524d928996323a3bdba
Guilherme Quentel Melo <guilherme@esss.com.br>
2017-04-11 10:47:29 -0300

    git bindings

--- scripts/fish/functions/fish_user_key_bindings.fish
+++ scripts/fish/functions/fish_user_key_bindings.fish
@@ -5,3 +5,11 @@ fzf_key_bindings
 
 # Show history with fzf and copy the selection to clipboard
 bind \er "history | fzf -m --bind enter:accept | xsel -i -b"
+
+set read_command "read -l result; and commandline -- \$result; commandline -f repaint"
+
+# Binds for git refs
+bind \cg\cb "git fshow branch | $read_command"
+bind \cg\cr "git fshow branch -r | $read_command"
+bind \cg\ct "git fshow tag --sort -version:refname | $read_command"
+bind \cg\ch "git fshow lgg --all | $read_command"
