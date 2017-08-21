bebbacdc0519d684c4531ea7ea337b5bbfa7da7f
Guilherme Quentel Melo <gqmelo@gmail.com>
2017-08-21 12:03:20 +0200

    Add home bin dir to PATH

--- scripts/fish/config_custom.fish
+++ scripts/fish/config_custom.fish
@@ -1,5 +1,10 @@
 if not contains ~/workspace/misc/bin $fish_user_paths
     set -U fish_user_paths ~/workspace/misc/bin $fish_user_paths
 end
+
+if not contains ~/bin $fish_user_paths
+    set -U fish_user_paths ~/bin $fish_user_paths
+end
+
 eval (python -m virtualfish)
 complete --command mu --wraps git
