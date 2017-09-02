4f765642ec1c9a4dae681e8408aa3a91c916ca99
Guilherme Quentel Melo <gqmelo@gmail.com>
2017-09-02 19:11:34 +0200

    Copy PID to clipboard and improve message

--- scripts/window-pid-notify.sh
+++ scripts/window-pid-notify.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 PID=`xprop _NET_WM_PID | cut -d " " -f3`
 echo $PID
-notify-send $PID
+echo $PID | xsel -i -b
+cmdline=`cat /proc/$PID/cmdline | tr '\0' ' '`
+
+notify-send "Process info" "PID: $PID
+cmdline: $cmdline"
 
