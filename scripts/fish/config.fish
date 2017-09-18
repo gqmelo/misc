7b71c89bf79da98614618768e19ac2445452948d
Guilherme Quentel Melo <gqmelo@gmail.com>
2017-09-18 14:03:44 +0200

    Improve bash and fish config files

--- scripts/fish/config.fish
+++ scripts/fish/config.fish
@@ -1,1 +1,5 @@
-. ~/workspace/misc/scripts/fish/config_custom.fish
+. ~/workspace/misc/scripts/fish/config_common.fish
+
+if test -e ~/workspace/Guilmelo/scripts/fish/config_work.fish
+    . ~/workspace/Guilmelo/scripts/fish/config_work.fish
+end
