323cc5fc6e76d1be8358bde4b862d0bcd96ded3f
Guilherme Quentel Melo <gqmelo@gmail.com>
2018-02-07 10:32:11 +0200

    Change PATH variable instead of fish_user_paths

--- scripts/fish/config_common.fish
+++ scripts/fish/config_common.fish
@@ -1,11 +1,11 @@
 . ~/miniconda3/etc/fish/conf.d/conda.fish
 
-if not contains ~/workspace/misc/bin $fish_user_paths
-    set -U fish_user_paths ~/workspace/misc/bin $fish_user_paths
+if not contains ~/workspace/misc/bin $PATH
+    set -gx PATH ~/workspace/misc/bin $PATH
 end
 
-if not contains ~/bin $fish_user_paths
-    set -U fish_user_paths ~/bin $fish_user_paths
+if not contains ~/bin $PATH
+    set -gx PATH ~/bin $PATH
 end
 
 eval (python -m virtualfish)
