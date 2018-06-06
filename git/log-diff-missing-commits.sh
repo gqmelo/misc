bdb205a784f842268a36ad249111ba9262f4ee6c
Guilherme Quentel Melo <gqmelo@gmail.com>
2018-06-06 13:54:45 +0200

    Add script to show the commits that are missing in the current branch

---
+++ git/log-diff-missing-commits.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+if [ ! -z "$1" ]; then
+    BRANCH="$1"
+elif git rev-parse origin/mainline > /dev/null 2>&1; then
+    BRANCH="origin/mainline"
+else
+    BRANCH="origin/master"
+fi
+
+git log \
+    --abbrev-commit \
+    --decorate \
+    --date=relative \
+    --format=format:'%C(bold blue)%h%C(reset) - %C(bold green)(%ar)%C(reset) %C(white)%s%C(reset) %C(dim white)- %an%C(reset)%C(bold yellow)%d%C(reset)' \
+    HEAD..$BRANCH
