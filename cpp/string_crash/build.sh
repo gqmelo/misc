86d80b735c8baea2870193ee72060c9f31a1dc3c
Guilherme Quentel Melo <gqmelo@gmail.com>
2019-01-02 11:02:12 +0200

    Add example of --version-script linker option

--- cpp/string_crash/build.sh
+++ cpp/string_crash/build.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 
-g++ -g -O2 -shared -fPIC -o libdummy_llvm.so dummy_llvm.cpp -static-libstdc++
+g++ -g -O2 -shared -fPIC -o libdummy_llvm.so dummy_llvm.cpp -static-libstdc++ -Wl,--version-script=dummy_llvm.map
+#g++ -g -O2 -shared -fPIC -o libdummy_llvm.so dummy_llvm.cpp -static-libstdc++
 g++ -g -O2 -o string_crash string_crash.cpp -ldummy_llvm -L. -Wl,-rpath='$ORIGIN'
