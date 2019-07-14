d4ed807903c8e1c4258de80be0f679c9c5a2da3b
Guilherme Quentel Melo <gqmelo@gmail.com>
2019-07-14 16:39:20 +0200

    Remove dotfiles

--- dotfiles/install.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-DOTFILES_DIR=$(readlink -f $(dirname ${BASH_SOURCE[0]}))
-
-echo "Installing files from '$DOTFILES_DIR'"
-
-ln -sf $DOTFILES_DIR/vimrc ~/.vimrc
-ln -sf $DOTFILES_DIR/vscode-settings.json ~/.config/Code/User/settings.json
-ln -sf $DOTFILES_DIR/vscode-keybindings.json ~/.config/Code/User/keybindings.json
