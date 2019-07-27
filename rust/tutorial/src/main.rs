70e1cfc57e9ed1447a0d629295de3962b3f7a992
Guilherme Quentel Melo <gqmelo@gmail.com>
2019-07-27 11:40:39 +0200

    Some more rust tutorial

--- rust/tutorial/src/main.rs
+++ rust/tutorial/src/main.rs
@@ -140,9 +140,58 @@ fn main() {
     let color = Color(0, 0, 0);
     println!("My color: {:#?}", color);
 
-    let rect1 = Rectangle { width: 10, height: 32};
+    let rect1 = Rectangle {
+        width: 30,
+        height: 50,
+    };
     println!("Rectangle: {:?}", rect1);
-    println!("Rectangle area: {}", rect1.area())
+    println!("Rectangle area: {}", rect1.area());
+
+    let rect2 = Rectangle {
+        width: 10,
+        height: 40,
+    };
+    let rect3 = Rectangle {
+        width: 60,
+        height: 45,
+    };
+
+    println!("Can rect1 hold rect2? {}", rect1.can_hold(&rect2));
+    println!("Can rect1 hold rect3? {}", rect1.can_hold(&rect3));
+
+    let square1 = Rectangle::square(4);
+    println!("Square: {:?}", square1);
+
+    let message1 = Message::Quit;
+    let message2 = Message::Move { x: 100, y: 90 };
+    let message3 = Message::Write(String::from("something"));
+    let message4 = Message::ChangeColor(255, 0, 255);
+
+    println!("Message: {:?}", message1);
+    println!("Message: {:?}", message2);
+    println!("Message: {:?}", message3);
+    println!("Message: {:?}", message4);
+
+    let some_number = Some(10);
+    let absent_number: Option<u32> = None;
+
+    fn process_optional_number(num: Option<u32>) {
+        match num {
+            Some(i) => println!("Got value {}", i),
+            None => println!("Got no value"),
+        };
+    }
+
+    process_optional_number(some_number);
+    process_optional_number(absent_number);
+}
+
+#[derive(Debug)]
+enum Message {
+    Quit,
+    Move { x: u32, y: u32 },
+    Write(String),
+    ChangeColor(i32, i32, i32),
 }
 
 #[derive(Debug)]
@@ -154,7 +203,7 @@ struct User {
 }
 
 #[derive(Debug)]
-struct Color(i32,i32,i32);
+struct Color(i32, i32, i32);
 
 #[derive(Debug)]
 struct Rectangle {
@@ -165,6 +214,17 @@ struct Rectangle {
 impl Rectangle {
     fn area(&self) -> u32 {
         self.width * self.height
+    }
+
+    fn can_hold(&self, rectangle: &Rectangle) -> bool {
+        self.width > rectangle.width && self.height > rectangle.height
+    }
+
+    fn square(size: u32) -> Rectangle {
+        Rectangle {
+            width: size,
+            height: size,
+        }
     }
 }
 
