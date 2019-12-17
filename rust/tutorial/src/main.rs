d5c4e61252395a783a557930230be10951713cf9
Guilherme Quentel Melo <gqmelo@gmail.com>
2019-12-17 23:20:31 +0200

    Rust tutorial: error handling

--- rust/tutorial/src/main.rs
+++ rust/tutorial/src/main.rs
@@ -1,3 +1,4 @@
+use std::fs::File;
 fn main() {
     println!("Hello, world!");
     another_function(5, 6);
@@ -292,6 +293,36 @@ fn main() {
     let score = scores.entry(String::from("Green")).or_insert(99);
     *score += 1;
     println!("Green: {:#?}", scores.get(&String::from("Green")));
+
+    // panic!("Everything is on fire!!!");
+    let v = vec![1, 2, 3];
+    // v[100];
+
+    // Error handling (Result)
+    use std::io::ErrorKind;
+    let f = File::open("/tmp/hello.txt");
+    let f = match f {
+        Ok(file) => file,
+        Err(error) => match error.kind() {
+            ErrorKind::NotFound => match File::create("/tmp/hello.txt") {
+                Ok(fc) => fc,
+                Err(e) => panic!("Could not create file {:?}", e),
+            },
+            other_error => panic!("Error opening file {:#?}", other_error),
+        },
+    };
+    // Both cause a panic
+    // let f2 = File::open("/tmp/hello2.txt").unwrap();
+    // let f2 = File::open("/tmp/hello2.txt").expect("Something happened");
+
+    let username = match read_username_from_file() {
+        Ok(value) => value,
+        Err(e) => {
+            println!("Could not read username: {:?}", e);
+            String::from("default_user")
+        }
+    };
+    println!("username: {}", username);
 }
 
 #[derive(Debug)]
@@ -383,4 +414,22 @@ mod sound {
     }
 
     fn breath_in() {}
+}
+
+use std::io;
+use std::io::Read;
+
+fn read_username_from_file() -> Result<String, io::Error> {
+    let f = File::open("hello.txt");
+
+    let mut f = match f {
+        Ok(file) => file,
+        Err(e) => return Err(e),
+    };
+
+    let mut s = String::new();
+    match f.read_to_string(&mut s) {
+        Ok(_) => Ok(s),
+        Err(e) => Err(e),
+    }
 }
