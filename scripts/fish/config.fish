b5c4ea099a3907fa964dc6dc0b93c90e0ced72a0
Guilherme Quentel Melo <gqmelo@gmail.com>
2025-06-04 12:10:13 +0200

    Move bash and fish files

--- scripts/fish/config.fish
+++
@@ -1,5 +0,0 @@
-. ~/workspace/misc/scripts/fish/config_common.fish
-
-if test -e ~/workspace/Guilmelo/scripts/fish/config_work.fish
-    . ~/workspace/Guilmelo/scripts/fish/config_work.fish
-end
--- scripts/install.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-DOTFILES_DIR=$(readlink -f $(dirname ${BASH_SOURCE[0]}))
-
-echo "Installing files from '$DOTFILES_DIR'"
-
-ln -sf $DOTFILES_DIR/bash/bashrc ~/.bashrc
-ln -sf $DOTFILES_DIR/fish/config.fish ~/.config/fish/config.fish
-ln -sf $DOTFILES_DIR/fish/functions ~/.config/fish/functions
